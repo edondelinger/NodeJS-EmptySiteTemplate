aa17c61b48cde6b83acb53bceb18f186735862d0
edondelinger <edondelinger@gmail.com>
2018-05-17 13:02:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello, world! Salut tout le monde !');
     
 }).listen(process.env.PORT || 8080);
